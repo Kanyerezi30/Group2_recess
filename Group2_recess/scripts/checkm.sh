6a38b75bfb90ac834f358554d259ad2c79820673
Kanyerezi30 <kanyerezi30@gmail.com>
2022-01-31 11:19:00 +0300

    contamination analysis

--- Group2_recess/scripts/checkm.sh
+++ Group2_recess/scripts/checkm.sh
@@ -8,3 +8,6 @@
 
 path='/etc/ace-data/home/skanyerezi/Group2_recess/Data/results'
 checkm taxonomy_wf species "Escherichia coli" ${path}/assembly checkmout -t 16 -x fasta  > ${path}/assembly_contamination/checkmoutput.tsv
+
+#tail -431 checkmoutput.tsv | head -429 | awk '$15 < 5 && $14 > 90' | awk '{print $1}' > contamination_5 #extract those with contamination
+head -138 ${path}/assembly_contamination/checkmoutput.tsv | tail -103 | awk '$15 < 10 && $14 > 80' | awk '{print $1}' > ${path}/assembly_contamination/passed_contamination_ids.txt # extract those with acceptable contamination levels
